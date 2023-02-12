fb70b57a336f9590cf927c500e1a2db7efa283bf
Prerit Das <preritdas@gmail.com>
2023-02-12 16:32:29 -0500

    Wrong movie name in test.

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -6,7 +6,7 @@ def test_movie():
     m = movie.Movie("top gun maverick")
 
     assert str(m)
-    assert m.movie_title == "Top Gun Maverick"
+    assert m.movie_title == "Top Gun: Maverick"
     assert all(actor in m.actors for actor in {"Tom Cruise", "Miles Teller"})
     assert all(director in m.directors for director in {"Joseph Kosinski"})
     assert m.duration == "2h 11m"
