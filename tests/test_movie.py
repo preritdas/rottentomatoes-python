059e7b7c91a44591a8a6b8500bc92630c35ecff8
Prerit Das <preritdas@gmail.com>
2023-02-12 16:10:44 -0500

    Update attrs for Maverick.

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -3,12 +3,12 @@ from rottentomatoes import exceptions
 
 
 def test_movie():
-    m = movie.Movie("top gun")
+    m = movie.Movie("top gun maverick")
 
     assert str(m)
     assert m.movie_title == "Top Gun"
     assert all(actor in m.actors for actor in {"Tom Cruise", "Miles Teller"})
-    assert all(director in m.directors for director in {"Tony Scott"})
+    assert all(director in m.directors for director in {"Joseph Kosinski"})
     assert m.duration == "1h 49m"
     assert all(genre in m.genres for genre in {"Action", "Adventure"})
-    assert m.rating == "PG"
+    assert m.rating == "PG-13"
