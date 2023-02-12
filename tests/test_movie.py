a5dd2cd81cb7894754143d19e4fd26fb1e4c214f
Prerit Das <preritdas@gmail.com>
2023-02-12 16:19:07 -0500

    Update Maverick movie length for movie tests.

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -9,6 +9,6 @@ def test_movie():
     assert m.movie_title == "Top Gun Maverick"
     assert all(actor in m.actors for actor in {"Tom Cruise", "Miles Teller"})
     assert all(director in m.directors for director in {"Joseph Kosinski"})
-    assert m.duration == "1h 49m"
+    assert m.duration == "2h 11m"
     assert all(genre in m.genres for genre in {"Action", "Adventure"})
     assert m.rating == "PG-13"
