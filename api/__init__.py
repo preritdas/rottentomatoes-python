24ae0a3d206415a6250690e0a401e083cafe0453
Prerit Das <preritdas@gmail.com>
2023-02-12 18:41:57 -0500

    Create API structure.

---
+++ api/__init__.py
@@ -0,0 +1,51 @@
+"""Basic API to interact with the rottentomatoes-python package."""
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+import rottentomatoes as rt
+
+
+class MovieQuery(BaseModel):
+    """Job request, querying for a movie."""
+    name: str = Field(..., title="Name of the movie you're searching for.")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "top gun"
+            }
+        }
+
+
+class MovieAttributes(BaseModel):
+    """Output, movie attributes."""
+    name: str = Field(..., title="Name of the movie according to its page.")
+    tomatometer: int = Field(..., title="Rotten Tomatoes Tomatometer.")
+    audience_score: int = Field(..., title="Rotten Tomatoes audience score.")
+    weighted_score: int = Field(
+        ..., 
+        title = "Internally formulated weighted score between tomatometer and audience score."
+    )
+    genres: list[str] = Field(..., title="List of genres.")
+    rating: str = Field(..., title="Movie viewership rating, ex. PG.")
+    duration: str = Field(..., title="String represented time, ex. 2h 11m.")
+    year: str = Field(..., title="String represented year, ex. 1995.")
+    actors: list[str] = Field(..., title="List of featured prominent actors.")
+    directors: list[str] = Field(..., title="List of directors.")
+
+
+app = FastAPI(
+    name = "Rotten Tomatoes Scraper API",
+    description = "Unofficial API getting data by scraping the Rotten Tomatoes website.",
+    version = "0.5.11"
+)
+
+
+@app.get("/movie/{movie_name}")
+async def movie_attributes(movie_name: str) -> MovieAttributes:
+    """Get a movie's attributes."""
+    movie = rt.Movie(movie_name)
+
+    return {
+        "name": movie.name,
+    }
