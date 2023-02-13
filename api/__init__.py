2000fd217b0f4e09c4ae49e5e551eeb2673d5f6b
Prerit Das <preritdas@gmail.com>
2023-02-13 01:09:49 +0000

    Separate models into a models module.

--- api/__init__.py
+++ api/__init__.py
@@ -1,65 +1,9 @@
 """Basic API to interact with the rottentomatoes-python package."""
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
 
 import rottentomatoes as rt
 
-
-class MovieQuery(BaseModel):
-    """Job request, querying for a movie."""
-    name: str = Field(..., title="Name of the movie you're searching for.")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "top gun"
-            }
-        }
-
-
-class MovieAttributes(BaseModel):
-    """Output, movie attributes."""
-    name: str = Field(..., title="Name of the movie according to its page.")
-    tomatometer: int = Field(..., title="Rotten Tomatoes Tomatometer.")
-    audience_score: int = Field(..., title="Rotten Tomatoes audience score.")
-    weighted_score: int = Field(
-        ..., 
-        title = "Internally formulated weighted score between tomatometer and audience score."
-    )
-    genres: list[str] = Field(..., title="List of genres.")
-    rating: str = Field(..., title="Movie viewership rating, ex. PG.")
-    duration: str = Field(..., title="String represented time, ex. 2h 11m.")
-    year: str = Field(..., title="String represented year, ex. 1995.")
-    actors: list[str] = Field(..., title="List of featured prominent actors.")
-    directors: list[str] = Field(..., title="List of directors.")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Bad Boys for Life",
-                "tomatometer": 76,
-                "audience_score": 96,
-                "weighted_score": 82,
-                "genres": [
-                    "Action",
-                    "Comedy"
-                ],
-                "rating": "R",
-                "duration": "2h 4m",
-                "year": "2020",
-                "actors": [
-                    "Will Smith",
-                    "Martin Lawrence",
-                    "Vanessa Hudgens",
-                    "Jacob Scipio",
-                    "Alexander Ludwig"
-                ],
-                "directors": [
-                    "Adil El Arbi",
-                    "Bilall Fallah"
-                ]
-            }
-        }
+from . import models
 
 
 app = FastAPI(
@@ -69,111 +13,7 @@ app = FastAPI(
 )
 
 
-class Movies(BaseModel):
-    movies: list[MovieAttributes] = Field(..., title="A list of movies with attributes.")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "movies": [
-                    {
-                    "name": "Bad Boys for Life",
-                    "tomatometer": 76,
-                    "audience_score": 96,
-                    "weighted_score": 82,
-                    "genres": [
-                        "Action",
-                        "Comedy"
-                    ],
-                    "rating": "R",
-                    "duration": "2h 4m",
-                    "year": "2020",
-                    "actors": [
-                        "Will Smith",
-                        "Martin Lawrence",
-                        "Vanessa Hudgens",
-                        "Jacob Scipio",
-                        "Alexander Ludwig"
-                    ],
-                    "directors": [
-                        "Adil El Arbi",
-                        "Bilall Fallah"
-                    ]
-                    },
-                    {
-                    "name": "Bad Boys II",
-                    "tomatometer": 23,
-                    "audience_score": 78,
-                    "weighted_score": 41,
-                    "genres": [
-                        "Action",
-                        "Comedy"
-                    ],
-                    "rating": "R",
-                    "duration": "2h 26m",
-                    "year": "2003",
-                    "actors": [
-                        "Martin Lawrence",
-                        "Will Smith",
-                        "Jordi Mollà",
-                        "Gabrielle Union",
-                        "Peter Stormare"
-                    ],
-                    "directors": [
-                        "Michael Bay"
-                    ]
-                    },
-                    {
-                    "name": "Bad Boys",
-                    "tomatometer": 43,
-                    "audience_score": 78,
-                    "weighted_score": 54,
-                    "genres": [
-                        "Action",
-                        "Comedy"
-                    ],
-                    "rating": "R",
-                    "duration": "1h 58m",
-                    "year": "1995",
-                    "actors": [
-                        "Martin Lawrence",
-                        "Will Smith",
-                        "Téa Leoni",
-                        "Tchéky Karyo",
-                        "Theresa Randle"
-                    ],
-                    "directors": [
-                        "Michael Bay"
-                    ]
-                    },
-                    {
-                    "name": "Bad Boys",
-                    "tomatometer": 90,
-                    "audience_score": 82,
-                    "weighted_score": 87,
-                    "genres": [
-                        "Drama"
-                    ],
-                    "rating": "R",
-                    "duration": "2h 3m",
-                    "year": "1983",
-                    "actors": [
-                        "Sean Penn",
-                        "Reni Santoni",
-                        "Esai Morales",
-                        "Jim Moody",
-                        "Ally Sheedy"
-                    ],
-                    "directors": [
-                        "Rick Rosenthal 2"
-                    ]
-                    }
-                ]
-            }
-        }
-
-
-def build_movie(movie_name: str = "", force_url: str = "") -> MovieAttributes:
+def build_movie(movie_name: str = "", force_url: str = "") -> models.MovieAttributes:
     """Construct a dictionary adhering to MovieAttributes."""
     if force_url:
         movie = rt.Movie(force_url=force_url)
@@ -201,7 +41,7 @@ async def test_homepage() -> str:
 
 
 @app.get("/movie/{movie_name}", tags=["General"])
-async def movie_attributes(movie_name: str) -> MovieAttributes:
+async def movie_attributes(movie_name: str) -> models.MovieAttributes:
     """Get a movie's attributes."""
     if "_" in movie_name:
         movie_name = movie_name.replace("_", " ")
@@ -210,7 +50,7 @@ async def movie_attributes(movie_name: str) -> MovieAttributes:
 
 
 @app.get("/search/{movie_name}", tags=["General"])
-async def multi_movie_search(movie_name: str) -> Movies:
+async def multi_movie_search(movie_name: str) -> models.Movies:
     """Search for the movie and return a list of valid results."""
     results = rt.search.filter_searches(results=rt.search.search_results(movie_name))
 
