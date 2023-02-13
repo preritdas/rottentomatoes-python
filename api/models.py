84d2c1c6300db64691a69d306a37d5ac84a9334d
Prerit Das <preritdas@gmail.com>
2023-02-13 01:10:28 +0000

    Reformat the example values.

--- api/models.py
+++ api/models.py
@@ -7,11 +7,7 @@ class MovieQuery(BaseModel):
     name: str = Field(..., title="Name of the movie you're searching for.")
 
     class Config:
-        schema_extra = {
-            "example": {
-                "name": "top gun"
-            }
-        }
+        schema_extra = {"example": {"name": "top gun"}}
 
 
 class MovieAttributes(BaseModel):
@@ -20,8 +16,8 @@ class MovieAttributes(BaseModel):
     tomatometer: int = Field(..., title="Rotten Tomatoes Tomatometer.")
     audience_score: int = Field(..., title="Rotten Tomatoes audience score.")
     weighted_score: int = Field(
-        ..., 
-        title = "Internally formulated weighted score between tomatometer and audience score."
+        ...,
+        title="Internally formulated weighted score between tomatometer and audience score.",
     )
     genres: list[str] = Field(..., title="List of genres.")
     rating: str = Field(..., title="Movie viewership rating, ex. PG.")
@@ -37,10 +33,7 @@ class MovieAttributes(BaseModel):
                 "tomatometer": 76,
                 "audience_score": 96,
                 "weighted_score": 82,
-                "genres": [
-                    "Action",
-                    "Comedy"
-                ],
+                "genres": ["Action", "Comedy"],
                 "rating": "R",
                 "duration": "2h 4m",
                 "year": "2020",
@@ -49,116 +42,94 @@ class MovieAttributes(BaseModel):
                     "Martin Lawrence",
                     "Vanessa Hudgens",
                     "Jacob Scipio",
-                    "Alexander Ludwig"
+                    "Alexander Ludwig",
                 ],
-                "directors": [
-                    "Adil El Arbi",
-                    "Bilall Fallah"
-                ]
+                "directors": ["Adil El Arbi", "Bilall Fallah"],
             }
         }
 
 
 class Movies(BaseModel):
-    movies: list[MovieAttributes] = Field(..., title="A list of movies with attributes.")
+    movies: list[MovieAttributes] = Field(
+        ..., title="A list of movies with attributes."
+    )
 
     class Config:
         schema_extra = {
             "example": {
                 "movies": [
                     {
-                    "name": "Bad Boys for Life",
-                    "tomatometer": 76,
-                    "audience_score": 96,
-                    "weighted_score": 82,
-                    "genres": [
-                        "Action",
-                        "Comedy"
-                    ],
-                    "rating": "R",
-                    "duration": "2h 4m",
-                    "year": "2020",
-                    "actors": [
-                        "Will Smith",
-                        "Martin Lawrence",
-                        "Vanessa Hudgens",
-                        "Jacob Scipio",
-                        "Alexander Ludwig"
-                    ],
-                    "directors": [
-                        "Adil El Arbi",
-                        "Bilall Fallah"
-                    ]
+                        "name": "Bad Boys for Life",
+                        "tomatometer": 76,
+                        "audience_score": 96,
+                        "weighted_score": 82,
+                        "genres": ["Action", "Comedy"],
+                        "rating": "R",
+                        "duration": "2h 4m",
+                        "year": "2020",
+                        "actors": [
+                            "Will Smith",
+                            "Martin Lawrence",
+                            "Vanessa Hudgens",
+                            "Jacob Scipio",
+                            "Alexander Ludwig",
+                        ],
+                        "directors": ["Adil El Arbi", "Bilall Fallah"],
                     },
                     {
-                    "name": "Bad Boys II",
-                    "tomatometer": 23,
-                    "audience_score": 78,
-                    "weighted_score": 41,
-                    "genres": [
-                        "Action",
-                        "Comedy"
-                    ],
-                    "rating": "R",
-                    "duration": "2h 26m",
-                    "year": "2003",
-                    "actors": [
-                        "Martin Lawrence",
-                        "Will Smith",
-                        "Jordi Mollà",
-                        "Gabrielle Union",
-                        "Peter Stormare"
-                    ],
-                    "directors": [
-                        "Michael Bay"
-                    ]
+                        "name": "Bad Boys II",
+                        "tomatometer": 23,
+                        "audience_score": 78,
+                        "weighted_score": 41,
+                        "genres": ["Action", "Comedy"],
+                        "rating": "R",
+                        "duration": "2h 26m",
+                        "year": "2003",
+                        "actors": [
+                            "Martin Lawrence",
+                            "Will Smith",
+                            "Jordi Mollà",
+                            "Gabrielle Union",
+                            "Peter Stormare",
+                        ],
+                        "directors": ["Michael Bay"],
                     },
                     {
-                    "name": "Bad Boys",
-                    "tomatometer": 43,
-                    "audience_score": 78,
-                    "weighted_score": 54,
-                    "genres": [
-                        "Action",
-                        "Comedy"
-                    ],
-                    "rating": "R",
-                    "duration": "1h 58m",
-                    "year": "1995",
-                    "actors": [
-                        "Martin Lawrence",
-                        "Will Smith",
-                        "Téa Leoni",
-                        "Tchéky Karyo",
-                        "Theresa Randle"
-                    ],
-                    "directors": [
-                        "Michael Bay"
-                    ]
+                        "name": "Bad Boys",
+                        "tomatometer": 43,
+                        "audience_score": 78,
+                        "weighted_score": 54,
+                        "genres": ["Action", "Comedy"],
+                        "rating": "R",
+                        "duration": "1h 58m",
+                        "year": "1995",
+                        "actors": [
+                            "Martin Lawrence",
+                            "Will Smith",
+                            "Téa Leoni",
+                            "Tchéky Karyo",
+                            "Theresa Randle",
+                        ],
+                        "directors": ["Michael Bay"],
                     },
                     {
-                    "name": "Bad Boys",
-                    "tomatometer": 90,
-                    "audience_score": 82,
-                    "weighted_score": 87,
-                    "genres": [
-                        "Drama"
-                    ],
-                    "rating": "R",
-                    "duration": "2h 3m",
-                    "year": "1983",
-                    "actors": [
-                        "Sean Penn",
-                        "Reni Santoni",
-                        "Esai Morales",
-                        "Jim Moody",
-                        "Ally Sheedy"
-                    ],
-                    "directors": [
-                        "Rick Rosenthal 2"
-                    ]
-                    }
+                        "name": "Bad Boys",
+                        "tomatometer": 90,
+                        "audience_score": 82,
+                        "weighted_score": 87,
+                        "genres": ["Drama"],
+                        "rating": "R",
+                        "duration": "2h 3m",
+                        "year": "1983",
+                        "actors": [
+                            "Sean Penn",
+                            "Reni Santoni",
+                            "Esai Morales",
+                            "Jim Moody",
+                            "Ally Sheedy",
+                        ],
+                        "directors": ["Rick Rosenthal 2"],
+                    },
                 ]
             }
         }
-        
