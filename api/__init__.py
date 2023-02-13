4210ecc3cd725019ba4b8ca915f5ab9cf7b244ea
Prerit Das <preritdas@gmail.com>
2023-02-12 20:04:38 -0500

    Examples for models.

--- api/__init__.py
+++ api/__init__.py
@@ -33,6 +33,34 @@ class MovieAttributes(BaseModel):
     actors: list[str] = Field(..., title="List of featured prominent actors.")
     directors: list[str] = Field(..., title="List of directors.")
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Bad Boys for Life",
+                "tomatometer": 76,
+                "audience_score": 96,
+                "weighted_score": 82,
+                "genres": [
+                    "Action",
+                    "Comedy"
+                ],
+                "rating": "R",
+                "duration": "2h 4m",
+                "year": "2020",
+                "actors": [
+                    "Will Smith",
+                    "Martin Lawrence",
+                    "Vanessa Hudgens",
+                    "Jacob Scipio",
+                    "Alexander Ludwig"
+                ],
+                "directors": [
+                    "Adil El Arbi",
+                    "Bilall Fallah"
+                ]
+            }
+        }
+
 
 app = FastAPI(
     title = "Rotten Tomatoes Scraper API",
@@ -43,6 +71,106 @@ app = FastAPI(
 
 class Movies(BaseModel):
     movies: list[MovieAttributes] = Field(..., title="A list of movies with attributes.")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "movies": [
+                    {
+                    "name": "Bad Boys for Life",
+                    "tomatometer": 76,
+                    "audience_score": 96,
+                    "weighted_score": 82,
+                    "genres": [
+                        "Action",
+                        "Comedy"
+                    ],
+                    "rating": "R",
+                    "duration": "2h 4m",
+                    "year": "2020",
+                    "actors": [
+                        "Will Smith",
+                        "Martin Lawrence",
+                        "Vanessa Hudgens",
+                        "Jacob Scipio",
+                        "Alexander Ludwig"
+                    ],
+                    "directors": [
+                        "Adil El Arbi",
+                        "Bilall Fallah"
+                    ]
+                    },
+                    {
+                    "name": "Bad Boys II",
+                    "tomatometer": 23,
+                    "audience_score": 78,
+                    "weighted_score": 41,
+                    "genres": [
+                        "Action",
+                        "Comedy"
+                    ],
+                    "rating": "R",
+                    "duration": "2h 26m",
+                    "year": "2003",
+                    "actors": [
+                        "Martin Lawrence",
+                        "Will Smith",
+                        "Jordi Mollà",
+                        "Gabrielle Union",
+                        "Peter Stormare"
+                    ],
+                    "directors": [
+                        "Michael Bay"
+                    ]
+                    },
+                    {
+                    "name": "Bad Boys",
+                    "tomatometer": 43,
+                    "audience_score": 78,
+                    "weighted_score": 54,
+                    "genres": [
+                        "Action",
+                        "Comedy"
+                    ],
+                    "rating": "R",
+                    "duration": "1h 58m",
+                    "year": "1995",
+                    "actors": [
+                        "Martin Lawrence",
+                        "Will Smith",
+                        "Téa Leoni",
+                        "Tchéky Karyo",
+                        "Theresa Randle"
+                    ],
+                    "directors": [
+                        "Michael Bay"
+                    ]
+                    },
+                    {
+                    "name": "Bad Boys",
+                    "tomatometer": 90,
+                    "audience_score": 82,
+                    "weighted_score": 87,
+                    "genres": [
+                        "Drama"
+                    ],
+                    "rating": "R",
+                    "duration": "2h 3m",
+                    "year": "1983",
+                    "actors": [
+                        "Sean Penn",
+                        "Reni Santoni",
+                        "Esai Morales",
+                        "Jim Moody",
+                        "Ally Sheedy"
+                    ],
+                    "directors": [
+                        "Rick Rosenthal 2"
+                    ]
+                    }
+                ]
+            }
+        }
 
 
 def build_movie(movie_name: str = "", force_url: str = "") -> MovieAttributes:
