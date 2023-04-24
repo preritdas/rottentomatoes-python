4613fe06ffcb2195cbae49fbaead2a29c1b00a32
Prerit Das <preritdas@gmail.com>
2023-04-24 17:38:47 +0000

    Dict from typing to support lower Python versions.

--- rottentomatoes/utils.py
+++ rottentomatoes/utils.py
@@ -1,6 +1,8 @@
 """Various utilities."""
+from typing import Dict
 
-REQUEST_HEADERS: dict[str, str] = {
+
+REQUEST_HEADERS: Dict[str, str] = {
     "User-Agent": (
         "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) "
         "Gecko/20100101 Firefox/12.0"
