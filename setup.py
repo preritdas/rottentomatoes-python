20c11c50917464dcc80adaba776b7140ffaa61bc
Prerit Das <preritdas@gmail.com>
2023-08-09 03:55:04 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = "0.5.18"
+VERSION = "0.6.0"
 DESCRIPTION = (
     "Scrape Rotten Tomatoes's website for basic information on movies, without the "
     "use of their hard-to-attain official REST API."
